16f4d584cf64426497fe9b0a82eb4e085936fcda
Sm00shed <149046260+Sm00shed@users.noreply.github.com>
2026-06-26 13:02:12 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -147,6 +147,7 @@
             unset CMAKE_TOOLCHAIN_FILE
 
             ${if isDarwin then ''
+              export MACOSX_DEPLOYMENT_TARGET="$(sw_vers -productVersion)"
               export LIBRARY_PATH="${pkgs.fontconfig.lib}/lib''${LIBRARY_PATH:+:$LIBRARY_PATH}"
               export LDFLAGS="-framework CoreText -framework CoreFoundation -framework CoreGraphics''${LDFLAGS:+ $LDFLAGS}"
               export NIX_LDFLAGS="-framework CoreText -framework CoreFoundation -framework CoreGraphics''${NIX_LDFLAGS:+ $NIX_LDFLAGS}"
